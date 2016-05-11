10ef6396614dbd563ecbcecedc7ad73dd86dce86
Gomgom <contact@gom2.net>
2016-05-11 23:23:05 +0900

    first release for version 1.3 (change admin parts -- change big part)

--- ISKRobot.py
+++ ISKRobot.py
@@ -2,54 +2,53 @@
  ISKRobot(Il-Su-KKun Robot) (일수꾼봇)
 
  Created by Gomgom (https://gom2.net)
- Final released: 2016-05-10
- Version: v1.2 updated
+ Final released: 2016-05-12
+ Version: v1.3.0
 """
 
 #
 # IMPORT PARTS
 #
-import sys, time, pickle
+import sys, time, pickle, os
 import logging
 from telegram.ext import Updater, CommandHandler
-from telegram import Emoji
+from telegram import Emoji, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardHide
 
 #
 # DEFINE PARTS
 #
-# exec has some information of command and instruction
-exec = (("추가", "[이름 금액] 순서\n\t\t\t빚을 추가합니다.\n\t\t\t마지막에 '-t 내용' 추가로 태그 추가 가능.\n\t\t\t단, 태그는 띄어쓰기 불가."), ("조회", "[ ]\n\t\t\t현재 빚 상황을 조회합니다."), ("명세", "[ ]\n\t\t\t최근 추가/삭제 내역을 조회합니다."),("부분", "[이름 금액] 순서\n\t\t\t부분 상환 금액을 입력합니다."), ("상환", "[이름] 순서\n\t\t\t목록을 삭제합니다."), ("초기화", "[ ]\n\t\t\t모든 빚을 초기화합니다."))
+# exec has some information of commands and instructions
+exec = (("추가", "[이름 금액 ... &내용]\n\t\t\t빚을 추가합니다.\n\t\t\t마지막에 '&내용' 추가 가능.\n\
+                \t\t\t단, 태그는 띄어쓰기 불가."),
+        ("조회", "[ ]\n\t\t\t현재 빚 상황을 조회합니다."),
+        ("명세", "[ ]\n\t\t\t최근 추가/삭제 내역을 조회합니다."),
+        ("부분", "[이름 금액 ...]\n\t\t\t부분 상환 금액을 입력합니다."),
+        ("상환", "[이름 ...]\n\t\t\t목록을 삭제합니다."),
+        ("초기화", "[ ]\n\t\t\t모든 빚을 초기화합니다."),
+        ("계좌", "[정보]\n\t\t\t계좌주, 은행, 번호등을 추가합니다."))
 userLedger = { }
 userStatement = { }
 TOKEN = ''
-ADMINID = ''
+
+# Checking parameters is or not, if there is, it's put in TOKEN(It's token of this bot))
+# Sample exec: python ISKRobot.py 12345678:A1B2C3D4E5F6G7H8i9_j10k11
+if len(sys.argv) != 2:
+    print("토큰이 입력되지 않았습니다. 매개변수에 TOKEN을 입력해 주세요.")
+    sys.exit()
+TOKEN = str(sys.argv[1])
+print("토큰 초기화가 완료되었습니다. 봇을 시작합니다.")
 
 # Check there is save or not
-with open("./debt.dat", 'rb') as f:
-    ledgerFromFile = pickle.load(f)
-    if userLedger != ledgerFromFile:
-        userLedger = ledgerFromFile
-with open("./state.dat", 'rb') as f:
-    stateFromFile = pickle.load(f)
-    if userStatement != stateFromFile:
-        userStatement = stateFromFile
-
-# Checking parameters are or not, if there are, they are put in TOKEN(It's token of this bot) and ADMINID(It's Telegram ID of administrator, if you need)
-# Sample exec: python ISKRobot.py 12345678:A1B2C3D4E5F6G7H8i9_j10k11 abcd1234
-if len(sys.argv) == 1:
-    print("토큰이 입력되지 않았습니다. 매개변수에 TOKEN, 관리자ID 순으로 입력해 주세요.")
-    sys.exit()
-if len(sys.argv) >= 2:
-    TOKEN = str(sys.argv[1])
-    if len(sys.argv) == 3:
-        ADMINID = sys.argv[2]
-        print("관리자가 등록되었습니다. 관리자 이외는 조회 기능만을 이용할 수 있습니다. 관리자는 %s 입니다." % ADMINID)
-    elif len(sys.argv) == 2:
-        ADMINID = '0'
-        print("관리자가 등록되지 않았습니다. 모든 사람이 추가, 상환 등의 이용이 가능합니다.")
-    else:
-        print("매개변수가 잘못되었습니다. 매개변수에 TOKEN, 관리자ID 순으로 입력해 주세요.")
-        sys.exit()
+if os.path.exists("./debt.dat"):
+    with open("./debt.dat", 'rb') as f:
+        ledgerFromFile = pickle.load(f)
+        if userLedger != ledgerFromFile:
+            userLedger = ledgerFromFile
+if os.path.exists("./state.dat"):
+    with open("./state.dat", 'rb') as f:
+        stateFromFile = pickle.load(f)
+        if userStatement != stateFromFile:
+            userStatement = stateFromFile
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -95,16 +94,19 @@ def start(bot, update):
     startMes += '여러분들이 빌린 빚을 갚게 하려고 늘 노력하고 있답니다. :)\n'
     startMes += '도움말이 필요하시면 /help를 입력해 주세요. ^^'
 
-    if str(update.message.chat_id) in userStatement.keys():
-        return bot.sendMessage(update.message.chat_id, text=startMes)
-    userStatement[str(update.message.chat_id)] = []
-    if str(update.message.chat_id) in userLedger.keys():
-        return bot.sendMessage(update.message.chat_id, text=startMes)
-    userLedger[str(update.message.chat_id)] = {}
-    memoryNow(str(update.message.chat_id))
-    startMes += '\n\n이 채팅/그룹에서는 이용이 처음이시네요. 새로운 ID가 만들어졌습니다.'
+    if not str(update.message.chat_id) in userLedger.keys():
+        userLedger[str(update.message.chat_id)] = {}
+        userStatement[str(update.message.chat_id)] = []
+        userLedger[str(update.message.chat_id)]['ADMINID'] = update.message.from_user.id
+        memoryNow(str(update.message.chat_id))
 
-    bot.sendMessage(update.message.chat_id, text=startMes)
+        startMes += '\n\n/start 명령을 실행하신 분이 관리자가 되셨습니다.\n'
+        startMes += '추가, 상환 등은 관리자만 이용 가능합니다.\n'
+        startMes += '/stop 명령으로 데이터베이스 삭제가 가능합니다.'
+
+        return bot.sendMessage(update.message.chat_id, text=startMes)
+    else:
+        return bot.sendMessage(update.message.chat_id, text=startMes + '\n\n이미 이 채팅에는 관리자가 존재합니다.')
 
 # It will show /help
 def help(bot, update):
@@ -116,13 +118,13 @@ def help(bot, update):
 
 # It will be performed when you type, /추가 사람이름 금액
 def input(bot, update, args):
-    if ADMINID != '0' and update.message.from_user.username != ADMINID: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+') # Checking you're owner or not
     if len(args) % 2 == 1: return bot.sendMessage(update.message.chat_id, text='올바르지 않은 입력입니다. /help를 참조해 주세요.') # Checking args number
     try: # For Catching it's number or not
         for i in range(0,(len(args) - 2) // 2): int(args[(i*2)+1])
         if (str(args[len(args) - 2]) != "-t"): int(args[len(args) - 1])
     except: return bot.sendMessage(update.message.chat_id, text='금액에는 숫자만 입력할 수 있습니다.')
 
+    # Check this chat room is registered, if not, just return
     ledger = {}
     statement = []
     checkUser = 0
@@ -133,9 +135,10 @@ def input(bot, update, args):
         checkUser = 1
     if checkUser == 0:
         return bot.sendMessage(update.message.chat_id, text='* 이 방에서 초기화가 되지 않았습니다.\n/start를 통해 초기화 후 이용해 주세요. *')
+    # Check she or he is room leader,
+    if userLedger[str(update.message.chat_id)].get('ADMINID', 0) != update.message.from_user.id: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+')
 
     length = len(args)
-
     if str(args[len(args) - 2]) == "-t": length = len(args) - 2
     for i in range(0,length // 2):
         count = 0
@@ -164,6 +167,7 @@ def input(bot, update, args):
 
 # It will be performed when you type, /명세, it shows your states.
 def latest(bot, update):
+    # Check this chat room is registered, if not, just return
     statement = []
     checkUser = 0
     if str(update.message.chat_id) in userLedger.keys():
@@ -181,6 +185,7 @@ def latest(bot, update):
 
 # It will be performed when you type, /조회
 def view(bot, update):
+    # Check this chat room is registered, if not, just return
     ledger = {}
     checkUser = 0
     if str(update.message.chat_id) in userLedger.keys():
@@ -191,19 +196,20 @@ def view(bot, update):
 
     result = "\n\n" + ("*" * 19) + "\n"
     for person in ledger.keys():
-        if person != 'recent':
+        if person != 'recent' and person != 'removeRequest' and person != 'ADMINID' and person != 'ACCOUNT':
             result = "  " + result + person + "\t\t\t" + makeNumToMoney(ledger[person]) + "원\n"
+    if ledger.get('ACCOUNT', '0') != '0': result = result + "\n" + ledger['ACCOUNT'] + "\n"
     result += "*" * 19
     bot.sendMessage(update.message.chat_id, text='잔금 조회입니다. (' + ledger['recent'] + ' 기준)' + result)
 
 # It will be performed when you type, /부분 사람이름 금액
 def returnP(bot, update, args):
-    if ADMINID != '0' and update.message.from_user.username != ADMINID: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+') # Checking you're owner or not
     if len(args) % 2 == 1: return bot.sendMessage(update.message.chat_id, text='올바르지 않은 입력입니다. /help를 참조해 주세요.') # Checking args number
     try: # For Catching it's number or not
         for i in range(0,len(args) // 2): int(args[(i*2)+1])
     except: return bot.sendMessage(update.message.chat_id, text='금액에는 숫자만 입력할 수 있습니다.')
 
+    # Check this chat room is registered, if not, just return
     ledger = {}
     statement = []
     checkUser = 0
@@ -214,6 +220,8 @@ def returnP(bot, update, args):
         checkUser = 1
     if checkUser == 0:
         return bot.sendMessage(update.message.chat_id, text='* 이 방에서 초기화가 되지 않았습니다.\n/start를 통해 초기화 후 이용해 주세요. *')
+    # Check she or he is room leader,
+    if userLedger[str(update.message.chat_id)].get('ADMINID', 0) != update.message.from_user.id: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+')
 
     for i in range(0,len(args) // 2):
         count = 0
@@ -241,8 +249,7 @@ def returnP(bot, update, args):
 
 # It will be performed when you type, /상환 사람이름
 def remove(bot, update, args):
-    if ADMINID != '0' and update.message.from_user.username != ADMINID: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+') # Checking you're owner or not
-
+    # Check this chat room is registered, if not, just return
     ledger = {}
     statement = []
     checkUser = 0
@@ -253,6 +260,8 @@ def remove(bot, update, args):
         checkUser = 1
     if checkUser == 0:
         return bot.sendMessage(update.message.chat_id, text='* 이 방에서 초기화가 되지 않았습니다.\n/start를 통해 초기화 후 이용해 주세요. *')
+    # Check she or he is room leader,
+    if userLedger[str(update.message.chat_id)].get('ADMINID', 0) != update.message.from_user.id: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+')
 
     delList = [] # Trick for remove various person
 
@@ -284,8 +293,7 @@ def remove(bot, update, args):
 
 # It will be performed when you type, /초기화
 def reset(bot, update):
-    if ADMINID != '0' and update.message.from_user.username != ADMINID: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+') # Checking you're owner or not
-
+    # Check this chat room is registered, if not, just return
     ledger = {}
     statement = []
     checkUser = 0
@@ -296,10 +304,12 @@ def reset(bot, update):
         checkUser = 1
     if checkUser == 0:
         return bot.sendMessage(update.message.chat_id, text='* 이 방에서 초기화가 되지 않았습니다.\n/start를 통해 초기화 후 이용해 주세요. *')
+    # Check she or he is room leader,
+    if userLedger[str(update.message.chat_id)].get('ADMINID', 0) != update.message.from_user.id: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+')
 
     ledger.clear()
 
-    state = "  ** " + exec[5][0] + " ** \n"
+    state = "  ** " + exec[5][0] + "가 수행됨. ** \n"
     if len(statement) < 10:
         statement.append(state)
     else:
@@ -310,6 +320,69 @@ def reset(bot, update):
     userStatement[str(update.message.chat_id)] = statement
     memoryNow(str(update.message.chat_id))
     bot.sendMessage(update.message.chat_id, text='초기화 작업을 완료했습니다.')
+
+def account(bot, update, args):
+    # Check this chat room is registered, if not, just return
+    ledger = {}
+    checkUser = 0
+    if str(update.message.chat_id) in userLedger.keys():
+        ledger = userLedger[str(update.message.chat_id)]
+        if not str(update.message.chat_id) in userStatement.keys(): userStatement[str(update.message.chat_id)] = []
+        checkUser = 1
+    if checkUser == 0:
+        return bot.sendMessage(update.message.chat_id, text='* 이 방에서 초기화가 되지 않았습니다.\n/start를 통해 초기화 후 이용해 주세요. *')
+    # Check she or he is room leader,
+    if userLedger[str(update.message.chat_id)].get('ADMINID', 0) != update.message.from_user.id: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+')
+
+    accountn = ''
+    for i in range(0,len(args)):
+        accountn += str(args[i]) + ' '
+    ledger['ACCOUNT'] = accountn
+
+    userLedger[str(update.message.chat_id)] = ledger
+    bot.sendMessage(update.message.chat_id, text='계좌가 기록되었습니다.')
+
+def stop(bot, update):
+    # Check this chat room is registered, if not, just return
+    checkUser = 0
+    if str(update.message.chat_id) in userLedger.keys():
+        if not str(update.message.chat_id) in userStatement.keys(): userStatement[str(update.message.chat_id)] = []
+        checkUser = 1
+    if checkUser == 0:
+        return bot.sendMessage(update.message.chat_id, text='* 이 방에서 초기화가 되지 않았습니다.\n/start를 통해 초기화 후 이용해 주세요. *')
+    # Check she or he is room leader,
+    if userLedger[str(update.message.chat_id)].get('ADMINID', 0) != update.message.from_user.id: return bot.sendMessage(update.message.chat_id, text='내 주인님이 아니에요..-_-+')
+
+    checkUser = 0
+    if str(update.message.chat_id) in userLedger.keys():
+        if not str(update.message.chat_id) in userStatement.keys(): userStatement[str(update.message.chat_id)] = []
+        checkUser = 1
+    if checkUser == 0:
+        return bot.sendMessage(update.message.chat_id, text='* 이 방에서 초기화가 되지 않았습니다.\n/start를 통해 초기화 후 이용해 주세요. *')
+
+    if userLedger[str(update.message.chat_id)].get('removeRequest', 0) == 0:
+        custom_keyboard = [[KeyboardButton("/confirm"), KeyboardButton("/cancel") ]]
+        reply_markup = ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard='True')
+        bot.sendMessage(chat_id=update.message.from_user.id, text='정말로 초기화하시겠습니까?', reply_markup=reply_markup)
+    else:
+        return bot.sendMessage(update.message.chat_id, text='일수꾼봇이 개인적으로 보낸 채팅을 확인해주세요.')
+
+    userLedger[str(update.message.chat_id)]['removeRequest'] = str(update.message.from_user.id)
+
+def confirm(bot, update):
+    for i in userLedger.keys():
+        if userLedger[i].get('removeRequest', '0') == str(update.message.from_user.id):
+            del userLedger[i]
+            del userStatement[i]
+            reply_markup = ReplyKeyboardHide()
+            return bot.sendMessage(chat_id=update.message.chat_id, text='이용을 중지합니다. 감사합니다.\n재이용은 이용하실 방에서 /start를 입력해 주세요.', reply_markup=reply_markup)
+
+def cancel(bot, update):
+    for i in userLedger.keys():
+        if userLedger[i].get('removeRequest', '0') == str(update.message.from_user.id):
+            userLedger[i]['removeRequest'] = 0
+            reply_markup = ReplyKeyboardHide()
+            return bot.sendMessage(chat_id=update.message.chat_id, text='취소되었습니다.', reply_markup=reply_markup)
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -330,6 +403,10 @@ def main():
     dp.addHandler(CommandHandler(exec[3][0], returnP, pass_args=True))
     dp.addHandler(CommandHandler(exec[4][0], remove, pass_args=True))
     dp.addHandler(CommandHandler(exec[5][0], reset))
+    dp.addHandler(CommandHandler(exec[6][0], account, pass_args=True))
+    dp.addHandler(CommandHandler("stop", stop))
+    dp.addHandler(CommandHandler("confirm", confirm))
+    dp.addHandler(CommandHandler("cancel", cancel))
     dp.addErrorHandler(error)
 
     updater.start_polling()
